c8f4b06d679c6d289c079cfa336b22d84ab04eca
samtwum <stwum@ska.ac.za>
2019-07-15 08:08:40 +0200

    pin numpy to versions with py2 support<1.17

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     author_email='nmarais@ska.ac.za',
     license='GPL',
     packages=find_packages(exclude=['ez_setup']),
-    install_requires=['Nose>=0.11.0', 'traceback2', 'ansicolors', 'numpy'],
+    install_requires=['Nose>=0.11.0', 'traceback2', 'ansicolors', 'numpy<1.17'],
     url='',
     include_package_data=True,
     entry_points="""
