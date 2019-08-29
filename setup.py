d719b2c7f7bde55822d3c74a93ace1a31240c578
Mpho Mphego <mpho112@gmail.com>
2019-08-29 11:07:18 +0200

    Migrate nosekatreport to Python 3
    This PR does the following:
    
    - [x] Adds GPL license as per `setup.py`
    - [x] Add tox.ini, Jenkinsfile and setup.cfg

--- setup.py
+++ setup.py
@@ -6,39 +6,47 @@
 # THIS SOFTWARE MAY NOT BE COPIED OR DISTRIBUTED IN ANY FORM WITHOUT THE      #
 # WRITTEN PERMISSION OF SKA SA.                                               #
 ###############################################################################
-import sys
-
 from setuptools import setup, find_packages
 
 
 setup(
-    name='nosekatreport',
-    version='0.2',
-    description='Nose plugin for writing an annoted test report',
-    long_description=open('README.rst').read(),
-    author='Neilen Marais',
-    author_email='nmarais@ska.ac.za',
-    license='GPL',
-    packages=find_packages(exclude=['ez_setup']),
-    install_requires=['Nose>=0.11.0', 'traceback2', 'ansicolors', 'numpy'],
-    url='',
+    name="nosekatreport",
+    version="0.3",
+    description="Nose plugin for writing an annotated test report.",
+    long_description=open("README.rst").read(),
+    author="CAM team",
+    author_email="cam@ska.ac.za",
+    license="GPL",
+    packages=find_packages(exclude=["ez_setup"]),
+    install_requires=["Nose>=0.11.0", "traceback2", "ansicolors", "numpy"],
+    setup_requires=["katversion"],
+    use_katversion=True,
+    url="http://ska.ac.za/",
     include_package_data=True,
     entry_points="""
         [nose.plugins.0.10]
         nosekatreport = nosekatreport:KatReportPlugin
         """,
-    classifiers = [
-        'Development Status :: 2 - Pre-Alpha',
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'Natural Language :: English',
-        'License :: OSI Approved :: GNU General Public License (GPL)',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.5',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Topic :: Software Development :: Testing',
-        'Topic :: Software Development :: Documentation'
-        ],
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Software Development :: Testing",
+        "Topic :: Software Development :: Documentation",
+    ],
+    platforms=["OS Independent"],
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, <4",
+    test_suite="nose.collector",
 )
