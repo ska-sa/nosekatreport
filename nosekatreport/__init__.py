584e638c8a6c95a176d475531b35ed0872533e78
Anele Makhaba <amakhaba@ska.ac.za>
2019-09-06 12:09:56 +0200

    Fixed imports

--- nosekatreport/__init__.py
+++ nosekatreport/__init__.py
@@ -10,9 +10,6 @@ standard_library.install_aliases()
 # BEGIN VERSION CHECK
 # Get package version when locally imported from repo or via -e develop install
 
-from .decorators import *
-from .plugin import *
-
 try:
     import katversion as _katversion
 except ImportError:
@@ -22,3 +19,6 @@ except ImportError:
 else:
     __version__ = _katversion.get_version(__path__[0])
 # END VERSION CHECK
+
+from .decorators import *
+from .plugin import *
