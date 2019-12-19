73f7ec3e02e48e7c291a6c2054463ff5bd6c63d1
Mpho Mphego <mpho112@gmail.com>
2019-12-19 11:42:04 +0200

    Ran black on setup.py and updated classifiers and python versions.

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 # Copyright (c) 2017 National Research Foundation (South African Radio Astronomy Observatory)
 # BSD license - see LICENSE for details
-from __future__ import (absolute_import, division, print_function)
+from __future__ import absolute_import, division, print_function
 
 from os import path
 from setuptools import setup, find_packages
 
 this_directory = path.abspath(path.dirname(__file__))
-files = {'Readme': 'README.rst'}
+files = {"Readme": "README.rst"}
 long_description = ""
 for name, filename in files.items():
     long_description = "{}{}\n".format(long_description, name)
@@ -18,7 +18,7 @@ setup(
     name="nosekatreport",
     description="Nose plugin for writing an annotated test report",
     long_description=long_description,
-    long_description_content_type='text/markdown',
+    long_description_content_type="text/markdown",
     author="cam",
     author_email="cam@ska.ac.za",
     license="BSD",
@@ -30,35 +30,36 @@ setup(
         nosekatreport = nosekatreport:KatReportPlugin
         """,
     classifiers=[
-        "Development Status :: 2 - Pre-Alpha",
+        "Development Status :: 5 - Production/Stable",
         "Environment :: Console",
         "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
         "Natural Language :: English",
-        "License :: OSI Approved :: GNU General Public License (GPL)",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.5",
-        "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.3",
-        "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Software Development :: Libraries :: Python Modules",
         "Topic :: Software Development :: Testing",
-        "Topic :: Software Development :: Documentation"
-        ],
+    ],
     platforms=["OS Independent"],
     keywords="meerkat kat ska",
-    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, <4",
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*",
     setup_requires=["katversion"],
     use_katversion=True,
     install_requires=[
         "future",
         "futures; python_version<'3'",
-        "Nose>=0.11.0", "traceback2",
-        "ansicolors", "numpy"
+        "Nose>=0.11.0",
+        "traceback2",
+        "ansicolors",
+        "numpy",
     ],
     zip_safe=False,
     test_suite="nose.collector",
